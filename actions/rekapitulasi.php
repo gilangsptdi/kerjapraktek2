dc89b330e96302054d38a76272477a09bc877168
raldyachmad <achmad.180903@gmail.com>
2024-06-30 20:58:01 +0700

    setting rekapitulasi

--- actions/rekapitulasi.php
+++ actions/rekapitulasi.php
@@ -1,2 +1,47 @@
 <?php
 include('../include/config.php');
+$sql = "SELECT 
+    COUNT(hemoglobin) AS total_hemoglobin,
+    COUNT(led) AS total_led,
+    COUNT(golongandarah) AS total_golongandarah,
+    COUNT(leukosittt) + COUNT(eritrosittt) AS total_urin_lengkap,
+    COUNT(protein) AS total_protein,
+    COUNT(teskehamilan) AS total_teskehamilan,
+    COUNT(antihiv) AS total_antihiv,
+    COUNT(sifilistprapid) AS total_sifilistprapid,
+    COUNT(hbsag) AS total_hbsag,
+    COUNT(antihivr2) AS total_antihivr2,
+    COUNT(antihivr3) AS total_antihivr3,
+    COUNT(styphio) + COUNT(sparatyphiao) + COUNT(sparatyphibo) + COUNT(sparatyphico) + 
+    COUNT(styphih) + COUNT(sparatyphiah) + COUNT(sparatyphibh) + COUNT(sparatyphich) AS total_widal,
+    COUNT(ns1dbd) AS total_ns1dbd,
+    COUNT(igmdbd) AS total_igmdbd,
+    COUNT(iggdbd) AS total_iggdbd,
+    COUNT(guladarahsewaktu) + COUNT(guladarahpuasa) + COUNT(guladarah2jamp) AS total_guladarah,
+    COUNT(kolesteroltotal) AS total_kolesteroltotal,
+    COUNT(asamurat) AS total_asamurat
+FROM laporan_lab
+WHERE tanggal = '$tgl';";
+$query = mysqli_query($conn, $sql);
+if (mysqli_num_rows($query) > 0) {
+    while ($data = mysqli_fetch_array($query)) {
+        $total_hemoglobin = $data['total_hemoglobin'];
+        $total_led = $data['total_led'];
+        $total_golongandarah = $data['total_golongandarah'];
+        $total_urin_lengkap = $data['total_urin_lengkap'];
+        $total_protein = $data['total_protein'];
+        $total_teskehamilan = $data['total_teskehamilan'];
+        $total_antihiv = $data['total_antihiv'];
+        $total_sifilistprapid = $data['total_sifilistprapid'];
+        $total_hbsag = $data['total_hbsag'];
+        $total_antihivr2 = $data['total_antihivr2'];
+        $total_antihivr3 = $data['total_antihivr3'];
+        $total_widal = $data['total_widal'];
+        $total_ns1dbd = $data['total_ns1dbd'];
+        $total_igmdbd = $data['total_igmdbd'];
+        $total_iggdbd = $data['total_iggdbd'];
+        $total_guladarah = $data['total_guladarah'];
+        $total_kolesteroltotal = $data['total_kolesteroltotal'];
+        $total_asamurat = $data['total_asamurat'];
+    }
+}
--- rekapitulasi/cetak.php
+++ rekapitulasi/cetak.php
@@ -27,6 +27,7 @@
     }
     th, td{
         padding: 0 4px;
+        text-align: center;
     }
     #second-row th {
     writing-mode: vertical-rl;
@@ -119,99 +120,37 @@
 
         for ($day = 1; $day <= $daysInMonth; $day++) {
             $date = DateTime::createFromFormat('Y-m-d', "$year-$month-$day");
-            $hariIni = $date->format('Y-m-d');
+            $tgl = $date->format('Y-m-d');
             include('../include/config.php');
-            $sql = "SELECT tanggal, 
-            COUNT(*) AS total_records, 
-            SUM(CASE WHEN hemoglobin IS NOT NULL AND hemoglobin <> '' THEN 1 ELSE 0 END) AS filled_hemoglobin, 
-            SUM(CASE WHEN leukosit IS NOT NULL AND leukosit <> '' THEN 1 ELSE 0 END) AS filled_leukosit,
-            SUM(CASE WHEN trombosit IS NOT NULL AND trombosit <> '' THEN 1 ELSE 0 END) AS filled_trombosit,
-            SUM(CASE WHEN eritrosit IS NOT NULL AND eritrosit <> '' THEN 1 ELSE 0 END) AS filled_eritrosit,
-            SUM(CASE WHEN hematokrit IS NOT NULL AND hematokrit <> '' THEN 1 ELSE 0 END) AS filled_hematokrit,
-            SUM(CASE WHEN limfosit IS NOT NULL AND limfosit <> '' THEN 1 ELSE 0 END) AS filled_limfosit,
-            SUM(CASE WHEN monosit IS NOT NULL AND monosit <> '' THEN 1 ELSE 0 END) AS filled_monosit,
-            SUM(CASE WHEN granulosit IS NOT NULL AND granulosit <> '' THEN 1 ELSE 0 END) AS filled_granulosit,
-            SUM(CASE WHEN led IS NOT NULL AND led <> '' THEN 1 ELSE 0 END) AS filled_led,
-            SUM(CASE WHEN golongandarah IS NOT NULL AND golongandarah <> '' THEN 1 ELSE 0 END) AS filled_golongandarah,
-            SUM(CASE WHEN warna IS NOT NULL AND warna <> '' THEN 1 ELSE 0 END) AS filled_warna,
-            SUM(CASE WHEN kejernihan IS NOT NULL AND kejernihan <> '' THEN 1 ELSE 0 END) AS filled_kejernihan,
-            SUM(CASE WHEN ph IS NOT NULL AND ph <> '' THEN 1 ELSE 0 END) AS filled_ph,
-            SUM(CASE WHEN beratjenis IS NOT NULL AND beratjenis <> '' THEN 1 ELSE 0 END) AS filled_beratjenis,
-            SUM(CASE WHEN protein IS NOT NULL AND protein <> '' THEN 1 ELSE 0 END) AS filled_protein,
-            SUM(CASE WHEN glukosa IS NOT NULL AND glukosa <> '' THEN 1 ELSE 0 END) AS filled_glukosa,
-            SUM(CASE WHEN bilirubin IS NOT NULL AND bilirubin <> '' THEN 1 ELSE 0 END) AS filled_bilirubin,
-            SUM(CASE WHEN urobilinogen IS NOT NULL AND urobilinogen <> '' THEN 1 ELSE 0 END) AS filled_urobilinogen,
-            SUM(CASE WHEN keton IS NOT NULL AND keton <> '' THEN 1 ELSE 0 END) AS filled_keton,
-            SUM(CASE WHEN nitrit IS NOT NULL AND nitrit <> '' THEN 1 ELSE 0 END) AS filled_nitrit,
-            SUM(CASE WHEN leukosittt IS NOT NULL AND leukosittt <> '' THEN 1 ELSE 0 END) AS filled_leukosittt,
-            SUM(CASE WHEN eritrosittt IS NOT NULL AND eritrosittt <> '' THEN 1 ELSE 0 END) AS filled_eritrosittt,
-            SUM(CASE WHEN eritrositt IS NOT NULL AND eritrositt <> '' THEN 1 ELSE 0 END) AS filled_eritrositt,
-            SUM(CASE WHEN leukositt IS NOT NULL AND leukositt <> '' THEN 1 ELSE 0 END) AS filled_leukositt,
-            SUM(CASE WHEN epitel IS NOT NULL AND epitel <> '' THEN 1 ELSE 0 END) AS filled_epitel,
-            SUM(CASE WHEN kristal IS NOT NULL AND kristal <> '' THEN 1 ELSE 0 END) AS filled_kristal,
-            SUM(CASE WHEN silinder IS NOT NULL AND silinder <> '' THEN 1 ELSE 0 END) AS filled_silinder,
-            SUM(CASE WHEN teskehamilan IS NOT NULL AND teskehamilan <> '' THEN 1 ELSE 0 END) AS filled_teskehamilan,
-            SUM(CASE WHEN pagi IS NOT NULL AND pagi <> '' THEN 1 ELSE 0 END) AS filled_pagi,
-            SUM(CASE WHEN sewaktu IS NOT NULL AND sewaktu <> '' THEN 1 ELSE 0 END) AS filled_sewaktu,
-            SUM(CASE WHEN antihiv IS NOT NULL AND antihiv <> '' THEN 1 ELSE 0 END) AS filled_antihiv,
-            SUM(CASE WHEN sifilistprapid IS NOT NULL AND sifilistprapid <> '' THEN 1 ELSE 0 END) AS filled_sifilistprapid,
-            SUM(CASE WHEN hbsag IS NOT NULL AND hbsag <> '' THEN 1 ELSE 0 END) AS filled_hbsag,
-            SUM(CASE WHEN antihivr2 IS NOT NULL AND antihivr2 <> '' THEN 1 ELSE 0 END) AS filled_antihivr2,
-            SUM(CASE WHEN antihivr3 IS NOT NULL AND antihivr3 <> '' THEN 1 ELSE 0 END) AS filled_antihivr3,
-            SUM(CASE WHEN styphio IS NOT NULL AND styphio <> '' THEN 1 ELSE 0 END) AS filled_styphio,
-            SUM(CASE WHEN sparatyphiao IS NOT NULL AND sparatyphiao <> '' THEN 1 ELSE 0 END) AS filled_sparatyphiao,
-            SUM(CASE WHEN sparatyphibo IS NOT NULL AND sparatyphibo <> '' THEN 1 ELSE 0 END) AS filled_sparatyphibo,
-            SUM(CASE WHEN sparatyphico IS NOT NULL AND sparatyphico <> '' THEN 1 ELSE 0 END) AS filled_sparatyphico,
-            SUM(CASE WHEN styphih IS NOT NULL AND styphih <> '' THEN 1 ELSE 0 END) AS filled_styphih,
-            SUM(CASE WHEN sparatyphiah IS NOT NULL AND sparatyphiah <> '' THEN 1 ELSE 0 END) AS filled_sparatyphiah,
-            SUM(CASE WHEN sparatyphibh IS NOT NULL AND sparatyphibh <> '' THEN 1 ELSE 0 END) AS filled_sparatyphibh,
-            SUM(CASE WHEN sparatyphich IS NOT NULL AND sparatyphich <> '' THEN 1 ELSE 0 END) AS filled_sparatyphich,
-            SUM(CASE WHEN ns1dbd IS NOT NULL AND ns1dbd <> '' THEN 1 ELSE 0 END) AS filled_ns1dbd,
-            SUM(CASE WHEN iggdbd IS NOT NULL AND iggdbd <> '' THEN 1 ELSE 0 END) AS filled_iggdbd,
-            SUM(CASE WHEN igmdbd IS NOT NULL AND igmdbd <> '' THEN 1 ELSE 0 END) AS filled_igmdbd,
-            SUM(CASE WHEN guladarahsewaktu IS NOT NULL AND guladarahsewaktu <> '' THEN 1 ELSE 0 END) AS filled_guladarahsewaktu,
-            SUM(CASE WHEN guladarahpuasa IS NOT NULL AND guladarahpuasa <> '' THEN 1 ELSE 0 END) AS filled_guladarahpuasa,
-            SUM(CASE WHEN guladarah2jamp IS NOT NULL AND guladarah2jamp <> '' THEN 1 ELSE 0 END) AS filled_guladarah2jamp,
-            SUM(CASE WHEN kolesteroltotal IS NOT NULL AND kolesteroltotal <> '' THEN 1 ELSE 0 END) AS filled_kolesteroltotal,
-            SUM(CASE WHEN asamurat IS NOT NULL AND asamurat <> '' THEN 1 ELSE 0 END) AS filled_asamurat
-            FROM laporan_lab 
-            WHERE tanggal = '$hariIni'
-            GROUP BY tanggal 
-            ORDER BY hemoglobin, leukosit, trombosit";
-            $query = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($query) > 0) {
-                while ($data = mysqli_fetch_array($query)) {
-
-                }
-            }
+            include('../actions/rekapitulasi.php');
+            
             echo "<tr>";
             echo "<th style='text-align:center;'>".$date->format('d') . "</th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
-            echo "<th></th>";
+            echo "<td>".$total_hemoglobin."</td>";
+            echo "<td></td>";
+            echo "<td>".$total_led."</td>";
+            echo "<td>".$total_golongandarah."</td>";
+            echo "<td>".$total_urin_lengkap."</td>";
+            echo "<td>".$total_protein."</td>";
+            echo "<td>".$total_teskehamilan."</td>";
+            echo "<td></td>";
+            echo "<td></td>";
+            echo "<td>".$total_antihiv."</td>";
+            echo "<td>".$total_sifilistprapid."</td>";
+            echo "<td>".$total_hbsag."</td>";
+            echo "<td>".$total_antihivr2."</td>";
+            echo "<td>".$total_antihivr3."</td>";
+            echo "<td>".$total_widal."</td>";
+            echo "<td>".$total_ns1dbd."</td>";
+            echo "<td>".$total_igmdbd."</td>";
+            echo "<td>".$total_iggdbd."</td>";
+            echo "<td>".$total_guladarah."</td>";
+            echo "<td>!</td>";
+            echo "<td>!</td>";
+            echo "<td>".$total_kolesteroltotal."</td>";
+            echo "<td>".$total_asamurat."</td>";
+            echo "<td>!</td>";
+            echo "<td>!</td>";
             echo "</tr>";
         }
         echo "<tr>";
