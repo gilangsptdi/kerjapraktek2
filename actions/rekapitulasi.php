9e50cf8a9cfe46603cefe40593a8c8fb0ed53377
raldyachmad <achmad.180903@gmail.com>
2024-07-01 08:20:02 +0700

    fix cetak rekapitulasi

--- actions/rekapitulasi.php
+++ actions/rekapitulasi.php
@@ -21,7 +21,7 @@ $sql = "SELECT
     COUNT(DISTINCT CASE WHEN guladarah2jamp IS NOT NULL THEN no_registrasi END) AS total_guladarah2jamp,
     COUNT(DISTINCT CASE WHEN kolesteroltotal IS NOT NULL THEN no_registrasi END) AS total_kolesteroltotal,
     COUNT(DISTINCT CASE WHEN asamurat IS NOT NULL THEN no_registrasi END) AS total_asamurat,
-    COUNT(DISTINCT CASE WHEN (pagi IS NOT NULL OR sewaktu IS NOT NULL) THEN no_registrasi END) AS total_bta
+    COUNT(DISTINCT CASE WHEN pagi IS NOT NULL OR sewaktu IS NOT NULL THEN no_registrasi END) AS total_bta
     -- COUNT(DISTINCT CASE WHEN trigliserida IS NOT NULL THEN no_registrasi END) AS total_trigliserida,
     -- COUNT(DISTINCT CASE WHEN hdl IS NOT NULL THEN no_registrasi END) AS total_hdl
 FROM laporan_lab
@@ -30,27 +30,27 @@ WHERE tanggal = '$tgl';";
 $query = mysqli_query($conn, $sql);
 if (mysqli_num_rows($query) > 0) {
     while ($data = mysqli_fetch_array($query)) {
-        $total_hemoglobin = $data['total_hemoglobin'];
-        $total_led = $data['total_led'];
-        $total_golongandarah = $data['total_golongandarah'];
-        $total_urin_lengkap = $data['total_urin_lengkap'];
-        $total_protein = $data['total_protein'];
-        $total_teskehamilan = $data['total_teskehamilan'];
-        $total_antihiv = $data['total_antihiv'];
-        $total_sifilistprapid = $data['total_sifilistprapid'];
-        $total_hbsag = $data['total_hbsag'];
-        $total_antihivr2 = $data['total_antihivr2'];
-        $total_antihivr3 = $data['total_antihivr3'];
-        $total_widal = $data['total_widal'];
-        $total_ns1dbd = $data['total_ns1dbd'];
-        $total_igmdbd = $data['total_igmdbd'];
-        $total_iggdbd = $data['total_iggdbd'];
-        $total_guladarahsewaktu = $data['total_guladarahsewaktu'];
-        $total_guladarahpuasa = $data['total_guladarahpuasa'];
-        $total_guladarah2jamp = $data['total_guladarah2jamp'];
-        $total_kolesteroltotal = $data['total_kolesteroltotal'];
-        $total_asamurat = $data['total_asamurat'];
-        $total_bta = $data['total_bta'];
+        $total_hemoglobin = $data['total_hemoglobin'] == '0' ? '' : $data['total_hemoglobin'];
+        $total_led = $data['total_led'] == '0' ? '' : $data['total_led'];
+        $total_golongandarah = $data['total_golongandarah'] == '0' ? '' : $data['total_golongandarah'];
+        $total_urin_lengkap = $data['total_urin_lengkap'] == '0' ? '' : $data['total_urin_lengkap'];
+        $total_protein = $data['total_protein'] == '0' ? '' : $data['total_protein'];
+        $total_teskehamilan = $data['total_teskehamilan'] == '0' ? '' : $data['total_teskehamilan'];
+        $total_antihiv = $data['total_antihiv'] == '0' ? '' : $data['total_antihiv'];
+        $total_sifilistprapid = $data['total_sifilistprapid'] == '0' ? '' : $data['total_sifilistprapid'];
+        $total_hbsag = $data['total_hbsag'] == '0' ? '' : $data['total_hbsag'];
+        $total_antihivr2 = $data['total_antihivr2'] == '0' ? '' : $data['total_antihivr2'];
+        $total_antihivr3 = $data['total_antihivr3'] == '0' ? '' : $data['total_antihivr3'];
+        $total_widal = $data['total_widal'] == '0' ? '' : $data['total_widal'];
+        $total_ns1dbd = $data['total_ns1dbd'] == '0' ? '' : $data['total_ns1dbd'];
+        $total_igmdbd = $data['total_igmdbd'] == '0' ? '' : $data['total_igmdbd'];
+        $total_iggdbd = $data['total_iggdbd'] == '0' ? '' : $data['total_iggdbd'];
+        $total_guladarahsewaktu = $data['total_guladarahsewaktu'] == '0' ? '' : $data['total_guladarahsewaktu'];
+        $total_guladarahpuasa = $data['total_guladarahpuasa'] == '0' ? '' : $data['total_guladarahpuasa'];
+        $total_guladarah2jamp = $data['total_guladarah2jamp'] == '0' ? '' : $data['total_guladarah2jamp'];
+        $total_kolesteroltotal = $data['total_kolesteroltotal'] == '0' ? '' : $data['total_kolesteroltotal'];
+        $total_asamurat = $data['total_asamurat'] == '0' ? '' : $data['total_asamurat'];
+        $total_bta = $data['total_bta'] == '0' ? '' : $data['total_bta'];
         // $total_trigliserida = $data['total_trigliserida'];
         // $total_hdl = $data['total_hdl'];
     }
--- rekapitulasi/cetak.php
+++ rekapitulasi/cetak.php
@@ -28,6 +28,7 @@
     th, td{
         padding: 0 4px;
         text-align: center;
+        font-size: 11pt;
     }
     #second-row th {
     writing-mode: vertical-rl;
@@ -117,13 +118,64 @@
         $year = intval($year);
         $month = intval($month);
         $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+        $jumlah_hemoglobin = 0;
+        $jumlah_led = 0;
+        $jumlah_golongandarah = 0;
+        $jumlah_urin_lengkap = 0;
+        $jumlah_protein = 0;
+        $jumlah_teskehamilan = 0;
+        $jumlah_bta = 0;
+        $jumlah_antihiv = 0;
+        $jumlah_sifilistprapid = 0;
+        $jumlah_hbsag = 0;
+        $jumlah_antihivr2 = 0;
+        $jumlah_antihivr3 = 0;
+        $jumlah_widal = 0;
+        $jumlah_ns1dbd = 0;
+        $jumlah_igmdbd = 0;
+        $jumlah_iggdbd = 0;
+        $jumlah_guladarahsewaktu = 0;
+        $jumlah_guladarahpuasa = 0;
+        $jumlah_guladarah2jamp = 0;
+        $jumlah_kolesteroltotal = 0;
+        $jumlah_asamurat = 0;
 
         for ($day = 1; $day <= $daysInMonth; $day++) {
             $date = DateTime::createFromFormat('Y-m-d', "$year-$month-$day");
             $tgl = $_GET['bulan']."-".$date->format('d');
             include('../include/config.php');
             include('../actions/rekapitulasi.php');
+            $totals = [
+                $total_hemoglobin,
+                $total_led,
+                $total_golongandarah,
+                $total_urin_lengkap,
+                $total_protein,
+                $total_teskehamilan,
+                $total_bta,
+                $total_antihiv,
+                $total_sifilistprapid,
+                $total_hbsag,
+                $total_antihivr2,
+                $total_antihivr3,
+                $total_widal,
+                $total_ns1dbd,
+                $total_igmdbd,
+                $total_iggdbd,
+                $total_guladarahsewaktu,
+                $total_guladarahpuasa,
+                $total_guladarah2jamp,
+                $total_kolesteroltotal,
+                $total_asamurat
+            ];
             
+            $jumlah_total = 0;
+            
+            foreach ($totals as $total) {
+                if ($total != '') {
+                    $jumlah_total += $total;
+                }
+            }
             echo "<tr>";
             echo "<th style='text-align:center;'>".$date->format('d') . "</th>";
             echo "<td>".$total_hemoglobin."</td>";
@@ -151,33 +203,77 @@
             echo "<td>".$total_asamurat."</td>";
             echo "<td></td>";
             echo "<td></td>";
+            echo "<td>".$jumlah_total."</td>";
             echo "</tr>";
+            $jumlah_hemoglobin += ($total_hemoglobin != '') ? $total_hemoglobin : 0;
+            $jumlah_led += ($total_led != '') ? $total_led : 0;
+            $jumlah_golongandarah += ($total_golongandarah != '') ? $total_golongandarah : 0;
+            $jumlah_urin_lengkap += ($total_urin_lengkap != '') ? $total_urin_lengkap : 0;
+            $jumlah_protein += ($total_protein != '') ? $total_protein : 0;
+            $jumlah_teskehamilan += ($total_teskehamilan != '') ? $total_teskehamilan : 0;
+            $jumlah_bta += ($total_bta != '') ? $total_bta : 0;
+            $jumlah_antihiv += ($total_antihiv != '') ? $total_antihiv : 0;
+            $jumlah_sifilistprapid += ($total_sifilistprapid != '') ? $total_sifilistprapid : 0;
+            $jumlah_hbsag += ($total_hbsag != '') ? $total_hbsag : 0;
+            $jumlah_antihivr2 += ($total_antihivr2 != '') ? $total_antihivr2 : 0;
+            $jumlah_antihivr3 += ($total_antihivr3 != '') ? $total_antihivr3 : 0;
+            $jumlah_widal += ($total_widal != '') ? $total_widal : 0;
+            $jumlah_ns1dbd += ($total_ns1dbd != '') ? $total_ns1dbd : 0;
+            $jumlah_igmdbd += ($total_igmdbd != '') ? $total_igmdbd : 0;
+            $jumlah_iggdbd += ($total_iggdbd != '') ? $total_iggdbd : 0;
+            $jumlah_guladarahsewaktu += ($total_guladarahsewaktu != '') ? $total_guladarahsewaktu : 0;
+            $jumlah_guladarahpuasa += ($total_guladarahpuasa != '') ? $total_guladarahpuasa : 0;
+            $jumlah_guladarah2jamp += ($total_guladarah2jamp != '') ? $total_guladarah2jamp : 0;
+            $jumlah_kolesteroltotal += ($total_kolesteroltotal != '') ? $total_kolesteroltotal : 0;
+            $jumlah_asamurat += ($total_asamurat != '') ? $total_asamurat : 0;
         }
+        $jmlBaris = $jumlah_hemoglobin +
+            $jumlah_led +
+            $jumlah_golongandarah +
+            $jumlah_urin_lengkap +
+            $jumlah_protein +
+            $jumlah_teskehamilan +
+            $jumlah_bta +
+            $jumlah_antihiv +
+            $jumlah_sifilistprapid +
+            $jumlah_hbsag +
+            $jumlah_antihivr2 +
+            $jumlah_antihivr3 +
+            $jumlah_widal +
+            $jumlah_ns1dbd +
+            $jumlah_igmdbd +
+            $jumlah_iggdbd +
+            $jumlah_guladarahsewaktu +
+            $jumlah_guladarahpuasa +
+            $jumlah_guladarah2jamp +
+            $jumlah_kolesteroltotal +
+            $jumlah_asamurat;
+
         echo "<tr>";
         echo "<th style='text-align:center'>Jumlah</th>";
+        echo "<td>".$jumlah_hemoglobin."</td>";
         echo "<td></td>";
+        echo "<td>".$jumlah_led."</td>";
+        echo "<td>".$jumlah_golongandarah."</td>";
+        echo "<td>".$jumlah_urin_lengkap."</td>";
+        echo "<td>".$jumlah_protein."</td>";
+        echo "<td>".$jumlah_teskehamilan."</td>";
+        echo "<td>".$jumlah_bta."</td>";
         echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
-        echo "<td></td>";
+        echo "<td>".$jumlah_antihiv."</td>";
+        echo "<td>".$jumlah_sifilistprapid."</td>";
+        echo "<td>".$jumlah_hbsag."</td>";
+        echo "<td>".$jumlah_antihivr2."</td>";
+        echo "<td>".$jumlah_antihivr3."</td>";
+        echo "<td>".$jumlah_widal."</td>";
+        echo "<td>".$jumlah_ns1dbd."</td>";
+        echo "<td>".$jumlah_igmdbd."</td>";
+        echo "<td>".$jumlah_iggdbd."</td>";
+        echo "<td>".$jumlah_guladarahsewaktu."</td>";
+        echo "<td>".$jumlah_guladarahpuasa."</td>";
+        echo "<td>".$jumlah_guladarah2jamp."</td>";
+        echo "<td>".$jumlah_kolesteroltotal."</td>";
+        echo "<td>".$jumlah_asamurat."</td>";
         echo "<td></td>";
         echo "<td></td>";
         echo "<td></td>";
@@ -189,15 +285,35 @@
     <script>
     const jenisPemeriksaanTh = document.getElementById('jenis-pemeriksaan');
     const secondRowThCount = document.getElementById('second-row').getElementsByTagName('th').length;
-    console.log(secondRowThCount);
-    // Perbarui atribut colspan dengan nilai yang dihitung
     jenisPemeriksaanTh.colSpan = secondRowThCount;
-    // window.onload = function() {
-    //   window.print();
-    //   window.onafterprint = function() {
-    //     window.close();
-    //   };
-    // };
+
+    function calculateTotals() {
+            const table = document.getElementById('mytable');
+            const tbody = table.querySelector('tbody');
+            const tfoot = table.querySelector('tfoot tr');
+
+            const totals = Array.from({ length: 26 }, () => 0);
+
+            for (let row of tbody.rows) {
+                for (let i = 1; i < row.cells.length; i++) {
+                    totals[i - 1] += parseInt(row.cells[i].textContent);
+                }
+            }
+
+            for (let i = 1; i < tfoot.cells.length; i++) {
+                tfoot.cells[i].textContent = totals[i - 1];
+            }
+        }
+
+        // Panggil fungsi saat halaman selesai dimuat
+        window.onload = calculateTotals;
+
+    window.onload = function() {
+      window.print();
+      window.onafterprint = function() {
+        window.close();
+      };
+    };
   </script>
 </body>
 </html>
